761bc57a05977b044000fe632f02ce3d9e5c855c
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2021-09-27 08:16:09 +0200

    esercitazione 4 parte 2 per casa

---
+++ esercitazioni/4_input_moltiplicazione.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main() {
+	int a, b;
+	cout << "Inserisci il primo numero: ";
+	cin >> a;
+	cout << "Inserisci il secondo numero: ";
+	cin >> b;
+	cout << a << " * " << b << " = " << a*b << endl;
+	return 0;
+}
