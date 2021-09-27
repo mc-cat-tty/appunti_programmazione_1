478364a640848cedbc2605a7ecda9c9e6a73b241
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2021-09-27 08:09:02 +0200

    esercizio 2 per casa

---
+++ esercitazioni/2_input_int.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main() {
+	int a;
+	cout << "Inserisci il valore di a: ";
+	cin >> a;
+	cout << "a vale: " << a << endl;
+	return 0;
+}
