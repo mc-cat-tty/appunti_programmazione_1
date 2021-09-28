3ede2176ab8f02a63ea7c6f134a30eb98196cdb2
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2021-09-28 08:26:41 +0200

    improve diff tempo

--- esercitazioni/5_diff_tempo.cpp
+++ esercitazioni/5_diff_tempo.cpp
@@ -9,6 +9,8 @@ int main() {
 	cin >> min1 >> sec1;
 	cout << "Minuti e secondi secondo tempo? ";
 	cin >> min2 >> sec2;
-	cout << "Differenza: " << abs(min1*60+sec1-(min2*60+sec2)) << endl;
+	int res = abs(min1*60+sec1-(min2*60+sec2));
+	cout << "Differenza (sec): " << res << endl;
+	cout << "Differenza (min sec): " << (int)res/60 << " " <<  res%60 << endl;
 }
 
