bcbd85f6dc9f3da1884d60e31048838961b546e5
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2021-10-05 08:52:10 +0200

    add esercizio

---
+++ esercitazioni/download_from_server.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	// [kb/s]
+	const int	V1_MAX	= 200,	// if t <= 60 min
+				V1_MIN	= 50,	// if t > 60 min
+				V2		= 100;	// always
+	const int	V1_MAX_TIME_SECONDS = 60*60;
+	int dim;					// dimensione film [kb]
+	
+	cout << "Dimensione film [kb]: ";
+	cin >> dim;
+	
+	int t_v1_max = dim/V1_MAX;
+	if (t_v1_max <= 60)
+		cout << "Server 1 in " << t_v1_max/60;
+	else {
+		int max_dim_v1_max = V1_MAX*V1_MAX_TIME_SECONDS;
+		int dim_v1_min = dim-max_dim_v1_max;
+		int t_v1_min = dim_v1_min/V1_MIN;
+		int t_v2 = dim/V2;
+		if (V1_MAX_TIME_SECONDS+t_v1_min <= t_v2)
+			cout << "Server 1 in " << (V1_MAX_TIME_SECONDS+t_v1_min)/60;
+		else
+			cout << "Server 2 in " << t_v2/60;
+	}
+	cout << endl;
+
+	return 0;
+}
