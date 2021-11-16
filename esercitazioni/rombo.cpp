c08ceaefed70e80ac81b1435eda580c1d7a48e4d
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2021-11-16 01:10:40 +0100

    add es vecchi: rombo e triangolo

---
+++ esercitazioni/rombo.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	short unsigned int lb;  // lunghezza della base
+	do {
+		cout << "Inserisci la lunghezza (intera) del lato: "; cin >> lb;
+	} while (lb%2 == 0);
+	
+	short unsigned int lo = (lb-1)/2+1;  // lunghezza del lato obliquo (e altezza)
+	// cout << lo << endl;  // tracing
+	
+	int n_ast;  // numero di asterischi
+	int n_spaces;
+	for (int i=0; i<lo; i++) {
+		n_ast = 1 + i*2;
+		// cout << n_ast << endl;  // tracing
+		n_spaces = (lb - n_ast)/2;
+		// cout << n_spaces << endl;  // tracing
+		for (int j=0; j<lb; j++) {
+			if (j>=n_spaces && j<n_spaces+n_ast)
+				cout << "*";
+			else
+				cout << " ";
+		}
+		cout << endl;
+	}
+	for (int i=lo-2; i>=0; i--) {
+		n_ast = 1 + i*2;
+		// cout << n_ast << endl;  // tracing
+		n_spaces = (lb - n_ast)/2;
+		// cout << n_spaces << endl;  // tracing
+		for (int j=0; j<lb; j++) {
+			if (j>=n_spaces && j<n_spaces+n_ast)
+				cout << "*";
+			else
+				cout << " ";
+		}
+		cout << endl;
+	}
+	
+	return 0;
+}
